5ac1e65498fc60ea914c833cf7ca54bfd1fec185
ErdemHHO <75128279+ErdemHHO@users.noreply.github.com>
2022-12-30 14:13:01 +0300

    mission completed

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,10 @@ const express=require("express");
 
 const router = express.Router();
 
+const auth = require("../middlewares/auth");
 const adminController=require("../controllers/admin.js");
 
-router.get("/dashboard",adminController.dashboard);
+router.get("/dashboard",auth,adminController.dashboard);
 
 router.get("/sil/:id",adminController.silme);
 
